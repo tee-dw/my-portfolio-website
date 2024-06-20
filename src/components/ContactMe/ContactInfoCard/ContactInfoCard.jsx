b84ad994e08e05bc4c20e0d9ff89f29667b3d5b1
Tobie Tboltz <tboltz1808@gmail.com>
2024-06-20 10:28:43 +0100

    quick fix on contact-me

--- src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
+++ src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import "./ContactInfoCard.css"
 
 const ContactInfoCard = ({ iconUrl, text }) => {
+
   return (
     <div className='contact-details-card'>
-        <div className='icon'>
-            <img src={iconUrl} alt={text} />
-        </div>
-        <p>{text}</p>
+      <div className='icon'>
+        <img src={iconUrl} alt={text} />
+      </div>
+      <p>{text}</p>
     </div>
   )
 }
--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -4,20 +4,31 @@ import ContactInfoCard from './ContactInfoCard/ContactInfoCard';
 import ContactForm from './ContactForm/ContactForm';
 
 const ContactMe = () => {
+
+    // const handleImg = () => {
+    //     console.log('Image clicked');
+    //     window.location.href = 'mailto:tboltz1808@gmail.com';
+    // }
+
   return (
     <section className='contact-container'>
         <h5>Contact Me</h5>
 
         <div className='contact-content'>
             <div style={{flex: 1}}>
-                <ContactInfoCard
-                    iconUrl="./assets/images/email_icon.png"
-                    text="tboltz1808@gmail.com"
-                />
-                <ContactInfoCard
-                    iconUrl="./assets/images/github_icon.png"
-                    text="https://github.com/tee-dw/get-a-therapist"
-                />
+                <a href="mailto:tboltz1808@gmail.com">
+                    <ContactInfoCard
+                        iconUrl="./assets/images/email_icon.png"
+                        text="tboltz1808@gmail.com"     
+                    />
+                </a>
+
+                <a href="https://github.com/tee-dw/get-a-therapist">
+                    <ContactInfoCard
+                        iconUrl="./assets/images/github_icon.png"
+                        text="https://github.com/tee-dw/get-a-therapist"
+                    />
+                </a>
             </div>
             <div style={{flex: 1}}>
                 <ContactForm />

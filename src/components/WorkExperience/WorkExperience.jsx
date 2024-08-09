388787f4b045fc9276bce4688b706a8d138fb7ae
Tobie Tboltz <tboltz1808@gmail.com>
2024-08-09 18:11:35 +0100

    nav

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import "./WorkExperience.css"
 import { WORK_EXPERIENCE } from '../../utils/data'
 import ExperienceCard from './ExperienceCard/ExperienceCard'
